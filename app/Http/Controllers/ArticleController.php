e35f08818d7b6be56880bc3a93c9e130fa6857fd
Uğur Arıcı <m.ugurarici@gmail.com>
2022-03-27 11:33:33 +0300

    Setup base Articles page up and running

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
 use App\Models\Article;
+use Inertia\Inertia;
 
 class ArticleController extends Controller
 {
@@ -15,7 +16,11 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+
+        return Inertia::render('Articles/Index', [
+            'articles' => $articles,
+        ]);
     }
 
     /**
---
+++ resources/js/Pages/Articles/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Burası resources/js/Pages/Articles/Index.vue dosyasıdır.
+    <hr />
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        {{ article.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["articles"],
+};
+</script>
